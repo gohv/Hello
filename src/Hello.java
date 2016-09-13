e3649015d1d80e9fa000c40940a1e3d5f0191c53
porov <diamandiev@gmail.com>
2016-09-13 17:04:44 +0200

    test commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,15 +1,18 @@
 /**
  * Created by George on 9/7/2016.
  */
-public class Hello {
-    public static void main(String[] args) {
+public class Hello
+    {
+    public static void main(String[] args)
+    {
 
         // declaring some arrays:
         char[] hello = {'h','e','l','l','o'};
         char[] name = {'a','n','d','r','e','i'};
 
+        for (int i = 0; i < name.length; i++)
+        {
+            System.out.println(name[i]);
+        }
     }
-
-
-
 }
